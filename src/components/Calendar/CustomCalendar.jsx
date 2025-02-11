11a12e7d49566c1b6f59d341c449d36200e26f29
Gustavo Francisco <franguho@gmail.com>
2025-02-11 12:24:53 -0600

    Inicio componente calendario

---
+++ src/components/Calendar/CustomCalendar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+function CustomCalendar() {
+    const [date, setDate] = useState(new Date());
+
+    return (
+        <div className="container my-4 p-4 bg-light rounded shadow">
+            <h2 className="text-primary text-center">📅 Mi Calendario</h2>
+            <div className="d-flex justify-content-center">
+                <Calendar
+                    onChange={setDate}
+                    value={date}
+                    className="border p-2 rounded bg-white shadow-sm"
+                />
+            </div>
+            <p className="mt-3 text-center fs-5">
+                <strong>Fecha Seleccionada:</strong> 
+                <span className="text-success ms-2">{date.toDateString()}</span>
+            </p>
+        </div>
+    );
+}
+
+export default CustomCalendar;
